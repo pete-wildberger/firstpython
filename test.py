2c490ecef43fabb23bce6679b24c7281516f484d
Peter Wildberger <pete.wildberger@gmail.com>
2017-07-23 15:48:46 -0500

    age question

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print "Hello, World!"
+#!/usr/bin/python
+def checkName(name):
+    checkName = input("Are you " + name + "? ")
+
+    if checkName.lower() == "yes":
+        print("Hello ", name)
+    else:
+        print("We're sorry about that.")
+def checkAge():
+    age = input("How old are you?")
+    print("Peter is ", age, " years old")
+checkName("Peter")
+checkAge()
