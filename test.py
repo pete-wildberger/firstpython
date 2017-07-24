a50917df4e9dbb0be050036c16bedb26422bf8a8
Peter Wildberger <pete.wildberger@gmail.com>
2017-07-24 08:33:18 -0500

    while loop working but not translating after first run through

--- test.py
+++ test.py
@@ -1,28 +1,35 @@
 #!/usr/bin/python
+program_active = True
+while program_active:
+    pyg = 'ay'
 
-pyg = 'ay'
+    sentence = raw_input('Enter a sentence:')
+    if sentence == 'quit':
+        program_active = False
 
-sentence = raw_input('Enter a word:')
-output = []
+    output = []
 
-sentArr = sentence.split( )
+    sentArr = sentence.split( )
 
-def pigLatin(original):
-    if len(original) > 0 and original[0] in ('a', 'e', 'i', 'o', 'u') and original.isalpha():
-        word = original.lower()
-        new_word = word + pyg
-        return new_word
-    elif len(original) > 0 and original.isalpha():
-        word = original.lower()
-        vowel_indices = [idx for idx, ch in enumerate(original) if ch.lower() in 'aeiou']
-        splitIdx = vowel_indices[0]
-        first = original[0 : splitIdx]
-        new_word = word[splitIdx: len(word)] + first + pyg
-        return new_word
-    else:
-        print 'empty'
 
-for item in sentArr:
-    output.append(pigLatin(item))
-answer = ' '.join(output)
-print answer
+    def pigLatin(original):
+        if len(original) > 0 and original[0] in ('a', 'e', 'i', 'o', 'u') and original.isalpha():
+            word = original.lower()
+            new_word = word + pyg
+            return new_word
+        elif len(original) > 0 and original.isalpha():
+            word = original.lower()
+            vowel_indices = [idx for idx, ch in enumerate(original) if ch.lower() in 'aeiou']
+            splitIdx = vowel_indices[0]
+            first = original[0 : splitIdx]
+            new_word = word[splitIdx: len(word)] + first + pyg
+            return new_word
+        else:
+            print 'empty'
+
+    for item in sentArr:
+        output.append(pigLatin(item))
+        answer = ' '.join(output)
+    print answer
+    output = []
+    sentArr = []
