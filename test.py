44a8b571d13a0aaa451cd5897ca60d947d5cfd9f
Peter Wildberger <pete.wildberger@gmail.com>
2017-07-24 12:57:57 -0500

    sentences working fine

--- test.py
+++ test.py
@@ -1,5 +1,21 @@
 #!/usr/bin/python
 program_active = True
+
+def pigLatin(original):
+    if len(original) > 0 and original[0] in ('a', 'e', 'i', 'o', 'u') and original.isalpha():
+        word = original.lower()
+        new_word = word + pyg
+        return new_word
+    elif len(original) > 0 and original.isalpha():
+        word = original.lower()
+        vowel_indices = [idx for idx, ch in enumerate(original) if ch.lower() in 'aeiou']
+        splitIdx = vowel_indices[0]
+        first = original[0 : splitIdx]
+        new_word = word[splitIdx: len(word)] + first + pyg
+        return new_word
+    else:
+        print 'empty'
+
 while program_active:
     pyg = 'ay'
 
@@ -10,22 +26,6 @@ while program_active:
     output = []
 
     sentArr = sentence.split( )
-
-
-    def pigLatin(original):
-        if len(original) > 0 and original[0] in ('a', 'e', 'i', 'o', 'u') and original.isalpha():
-            word = original.lower()
-            new_word = word + pyg
-            return new_word
-        elif len(original) > 0 and original.isalpha():
-            word = original.lower()
-            vowel_indices = [idx for idx, ch in enumerate(original) if ch.lower() in 'aeiou']
-            splitIdx = vowel_indices[0]
-            first = original[0 : splitIdx]
-            new_word = word[splitIdx: len(word)] + first + pyg
-            return new_word
-        else:
-            print 'empty'
 
     for item in sentArr:
         output.append(pigLatin(item))
