378d691ee8340d296cb4f2e3e33f6632b5bd6a56
Peter Wildberger <pete.wildberger@gmail.com>
2017-07-23 22:25:39 -0500

    cleaned up

--- test.py
+++ test.py
@@ -1,27 +1,11 @@
 #!/usr/bin/python
 
-# def checkName(name):
-#     checkName = raw_input("Are you " + name + "? ")
-#
-#     if checkName.lower() == "yes":
-#         print("Hello ", name)
-#     else:
-#         print("We're sorry about that.")
-# def checkAge():
-#     age = input("How old are you?")
-#     print("Peter is ", age, " years old")
-# checkName("Peter")
-# checkAge()
-
 pyg = 'ay'
 
 sentence = raw_input('Enter a word:')
 output = []
 
 sentArr = sentence.split( )
-print sentArr
-
-
 
 def pigLatin(original):
     if len(original) > 0 and original[0] in ('a', 'e', 'i', 'o', 'u') and original.isalpha():
